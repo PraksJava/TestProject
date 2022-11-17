f84be3d61f834c5e099a8098487280e4cfc3ef63
praks.javaa@gmail.com <praks.javaa@gmail.com>
2022-11-17 11:54:37 +0530

    Test project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          
+        </p>
+        <h1>This is my first react app</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
